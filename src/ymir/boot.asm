e5753bb1b885c28eb68203546b7feb6a8935c8fb
Richard Remer <reanjr@gmail.com>
2016-04-24 20:26:27 -0700

    detect 64 bit support

--- src/ymir/boot.asm
+++ src/ymir/boot.asm
@@ -9,8 +9,31 @@ start:
     mov     sp, 0x7c00      ; start stack at bootloader address
     mov     bp, 0x7c00      ; start base pointer at stack pointer
 
-    mov     si, ident       ; message
-    call    outln
+    ; print bootloader identification
+    mov     si, ident       ; bootloader identification
+    call    outln           ; print message
+    
+    ; check for 64-bit support
+    mov     si, err_no64    ; message in case of error
+    mov     eax, 0x80000000 ; extended function support function
+    cpuid                   ; load caps into EAX
+    cmp     eax, 0x80000001 ; extended processor info function
+    jb      .error          ; no extended proc. info means no 64-bit support
+    
+    mov     eax, 0x80000001 ; extended processor info function
+    cpuid                   ; load processor info
+    test    edx, 1<<29      ; check LM bit
+    jz      .error          ; 64-bit unsupported
+
+    mov     si, cap64       ; indicate 64-bit support
+    call    outln           ; print message
+
+    jmp     .halt           ; terminate
+
+    .error:
+    call    outln           ; message expected in SI
+    
+    .halt:
     cli
     hlt
 
@@ -32,13 +55,12 @@ outln:
     call    out             ; pass along message
     mov     si, endln       ; message
     call    out             ; print endln
+    ret
 
-ident:
-    db      "Ymir v0.0.1"   ; bootloader identification
-    db      0x00            ; nul-terminated
-endln:
-    db      0xd,0xa         ; CR/LF
-    db      0x00            ; nul-terminated
+ident:      db  "Ymir v0.0.1",0x00
+cap64:      db  "64-bit support detected",0x00
+err_no64:   db  "64-bit support not detected",0x00
+endln:      db  0xd,0xa,0x00
 
 times 512 - ($ - $$) db 0x00; fill to 512 bytes with 0
 end:
